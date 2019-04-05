0d9718365189b2ef65fe7023bc6fd8f8384aeefc
Marat Enaliev <enamarat@yandex.ru>
2019-04-05 09:20:55 +0300

    Refactor code by making a separate component for landing page.

---
+++ inthespotlight/src/components/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+
+import LandingPage from './LandingPage.js'
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
+import '../styles/App.css';
+
+class App extends Component {
+  state = { users: []}
+
+  componentDidMount() {
+    // fetch('/users').
+    // then(res => res.json()).
+    // then(users => this.setState({users}))
+  }
+
+
+  render() {
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+        </Switch>
+      </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
+
+
+
+// <h1> Users </h1>
+// <ul>
+// {  this.state.users.map(user =>
+//   <li key={user.id}> {user.user}</li>
+// )}
+// </ul>
