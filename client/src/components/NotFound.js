0939c1cd50445bb4b9198a00317e11a010b1237d
Marat Enaliev <enamarat@yandex.ru>
2019-04-11 15:06:12 +0300

    Style Page-not-found component.

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Photo from "../images/baboo.jpg";
+
 
 const NotFound = () => {
-  return(
-    <div id="not-found">
-          <h3> Error 404: page not found</h3>
-          <p>The route you requested doesn't exist.</p>
 
-    </div>
-  );
+    return(
+      <div className="container" id="not-found">
+        <div className="row">
+          <div className="col col-12 col-md my-auto" id="not-found-header">
+            <h3> Error 404: page not found</h3>
+            <p>The route you requested doesn't exist.</p>
+          </div>
+          <div className="col" id="picture">
+            <img src={Photo} alt="baboo" id="baboo"/>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default NotFound;
