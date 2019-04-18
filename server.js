80c08acd2c83444e996d76bf5d604fa705e7df97
Marat Enaliev <enamarat@yandex.ru>
2019-04-18 18:41:49 +0300

    Delete code from server.js which crashes the app when it's deployed on Heroku.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+
 
 
 const morgan = require('morgan');
@@ -157,14 +157,7 @@ app.use(function(err, req, res, next) {
 });
 
 /**********/
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+
 /**********/
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
