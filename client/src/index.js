fec7ce494b0b7c44d9117aedb9a4c2669bf8daa5
Marat Enaliev <enamarat@gmail.com>
2020-10-02 11:14:34 +0300

    Switch from mlab to mongoDB Atlas.

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App.js';
-//import * as serviceWorker from './serviceWorker';
-
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/App.css';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
