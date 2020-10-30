658f92b1f5fe4af0b6d5228b2c2ee2e2ffbda930
Marat Enaliev <enamarat@gmail.com>
2020-10-30 13:02:44 +0300

    Extract routes from server.js and place them into a separate file.

---
+++ routes/routes.js
@@ -0,0 +1,132 @@
+const User = require('./models/user.js');
+const mongoose = require('mongoose');
+
+module.exports = function (app) {
+  app.post('/register', function(req, res, next) {
+    if (req.body.email && req.body.password) {
+      const userData = {
+        email: req.body.email,
+        password: req.body.password
+      };
+
+    // if email of a newly created user mathces email of an existing user, throw an error
+    User.create(userData, (err, user) => {
+      if(err) {
+        const err = new Error('The entered email already exists!');
+        err.status = 400;
+        res.send(err);
+      } else {
+        req.session.userId = user._id;
+        res.status(201);
+        res.send(req.session);
+        }
+    });
+    } else {
+      const err = new Error('All fields required!');
+      err.status = 400;
+      res.send(err);
+    }
+  });
+
+  app.post('/login', function(req, res, next) {
+    if (req.body.email && req.body.password) {
+      User.authenticate(req.body.email, req.body.password,
+      function(error, user) {
+        if (error || !user) {
+          console.log('Failure!');
+          const err = new Error('Wrong email or password.');
+          err.status = 401;
+          res.send(err);
+        } else {
+          console.log('success!');
+          req.session.userId = user._id;
+          res.send(req.session);
+        }
+      });
+    }
+  });
+
+
+  app.get('/profile', async function(req, res) {
+    if (!req.session.userId) {
+      const err = new Error("You are not authorized to view this page.");
+      err.status = 403;
+      res.send(err);
+    }
+
+    User.findById(req.session.userId)
+      .exec(async function(error, user) {
+        if(error) {
+          res.send(error);
+        } else {
+          const completeData = {
+            user: user
+          }
+          res.json(completeData);
+        }
+      });
+  });
+
+  app.get('/profile/:symbol', async function(req, res) {
+    const symbol = req.params.symbol;
+    await User.find({"watchedTickers.name": symbol}).exec(async function(err, results) {
+      if (err) {
+        console.log(err);
+      }
+
+      // if a ticker wasn't saved to a database previously
+      if (results.length === 0) {
+        // send an API request
+        const api_url = `https://cloud.iexapis.com/stable/stock/${symbol}/batch?types=quote,news,chart&range=1m&last=10&token=${process.env.REACT_APP_API_KEY}`;
+        const fetch_response = await fetch(api_url);
+        const json = await fetch_response.json();
+
+        // save ticker to a database
+         await User.findByIdAndUpdate(req.session.userId,
+         { $push: { watchedTickers: {name: symbol} } },
+         { new: true, upsert: true }
+        );
+        res.json(json);
+      } else {
+        const api_url = `https://cloud.iexapis.com/stable/stock/${symbol}/batch?types=quote,news,chart&range=1m&last=10&token=${process.env.REACT_APP_API_KEY}`;
+        const fetch_response = await fetch(api_url);
+        const json = await fetch_response.json();
+        res.json(json);
+      }
+    });
+
+  });
+
+  app.get('/profile/delete/:symbol', async function(req, res) {
+    const symbol = req.params.symbol.toLowerCase();
+    await User.findByIdAndUpdate(req.session.userId,
+      { $pull: { "watchedTickers": {"name": symbol} } },
+      { safe: true, multi: true }
+    );
+    const info = {
+      status: "ok"
+    }
+    res.json(info);
+  });
+
+  app.get('/profile/:symbol/historic-prices/:date', async function(req, res) {
+    const symbol = req.params.symbol;
+    const date = req.params.date;
+    const api_url = `https://cloud.iexapis.com/stable/stock/${symbol}/chart/date/${date}?token=${process.env.REACT_APP_API_KEY}`;
+    const fetch_response = await fetch(api_url);
+    const json = await fetch_response.json();
+    res.json(json);
+  });
+
+  app.get('/logout', function(req, res) {
+    req.session.destroy(function(err){
+      if(err){
+        console.log(err);
+        res.send(err);
+      } else {
+        res.send();
+      }
+    });
+  });
+
+}
--- server.js
+++ server.js
@@ -11,21 +11,15 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
 const app = express();
+const routes = require('./routes/routes.js');
 const port = process.env.PORT || 5000;
 
 /***************** Database ******************/
-const User = require('./models/user.js');
-
-// mongodb connection
 mongoose.connect(process.env.DB || "mongodb://localhost:27017/in-the-spotlight");
 const db = mongoose.connection;
-
-// database connection error
 db.on('error', (error) => {
   console.error("connection error:", error);
 });
-
-// message about successful connection to the database
 db.once('open', () => {
   console.log("database connection is successful!");
 });
@@ -41,8 +35,6 @@ app.use(session({
   unset: 'destroy'
 }));
 
-
-/***********************/
 //use cors to allow cross origin resource sharing
 app.use(
   cors({
@@ -57,136 +49,7 @@ app.use(morgan('dev'));
 
 /*********Routes******************/
 app.use('/', express.static(`${__dirname}/client/build`));
-
-app.post('/register', function(req, res, next) {
-  if (req.body.email && req.body.password) {
-    const userData = {
-      email: req.body.email,
-      password: req.body.password
-    };
-
-  // if email of a newly created users mathces email of a existing user, throw an error
-  User.create(userData, (err, user) => {
-    if(err) {
-      const err = new Error('The entered email already exists!');
-      err.status = 400;
-      res.send(err);
-    } else {
-      req.session.userId = user._id;
-      res.status(201);
-      res.send(req.session);
-      }
-  });
-  } else {
-    const err = new Error('All fields required!');
-    err.status = 400;
-    res.send(err);
-  }
-
-});
-
-
-app.post('/login', function(req, res, next) {
-  if (req.body.email && req.body.password) {
-    User.authenticate(req.body.email, req.body.password,
-    function(error, user) {
-      if (error || !user) {
-        console.log('Failure!');
-        const err = new Error('Wrong email or password.');
-        err.status = 401;
-        res.send(err);
-      } else {
-        console.log('success!');
-        req.session.userId = user._id;
-        res.send(req.session);
-      }
-    });
-  }
-});
-
-
-app.get('/profile', async function(req, res) {
-  if (!req.session.userId) {
-    const err = new Error("You are not authorized to view this page.");
-    err.status = 403;
-    res.send(err);
-  }
-
-  User.findById(req.session.userId)
-    .exec(async function(error, user) {
-      if(error) {
-        res.send(error);
-      } else {
-        const completeData = {
-          user: user
-        }
-        res.json(completeData);
-      }
-    });
-});
-
-app.get('/profile/:symbol', async function(req, res) {
-  const symbol = req.params.symbol;
-  await User.find({"watchedTickers.name": symbol}).exec(async function(err, results) {
-    if (err) {
-      console.log(err);
-    }
-
-    // if a ticker wasn't saved to a database previously
-    if (results.length === 0) {
-      // send an API request
-      const api_url = `https://cloud.iexapis.com/stable/stock/${symbol}/batch?types=quote,news,chart&range=1m&last=10&token=${process.env.REACT_APP_API_KEY}`;
-      const fetch_response = await fetch(api_url);
-      const json = await fetch_response.json();
-
-      // save ticker to a database
-       await User.findByIdAndUpdate(req.session.userId,
-       { $push: { watchedTickers: {name: symbol} } },
-       { new: true, upsert: true }
-      );
-      res.json(json);
-    } else {
-      const api_url = `https://cloud.iexapis.com/stable/stock/${symbol}/batch?types=quote,news,chart&range=1m&last=10&token=${process.env.REACT_APP_API_KEY}`;
-      const fetch_response = await fetch(api_url);
-      const json = await fetch_response.json();
-      res.json(json);
-    }
-  });
-
-});
-
-app.get('/profile/delete/:symbol', async function(req, res) {
-  const symbol = req.params.symbol.toLowerCase();
-  await User.findByIdAndUpdate(req.session.userId,
-    { $pull: { "watchedTickers": {"name": symbol} } },
-    { safe: true, multi: true }
-  );
-  const info = {
-    status: "ok"
-  }
-  res.json(info);
-});
-
-app.get('/profile/:symbol/historic-prices/:date', async function(req, res) {
-  const symbol = req.params.symbol;
-  const date = req.params.date;
-  const api_url = `https://cloud.iexapis.com/stable/stock/${symbol}/chart/date/${date}?token=${process.env.REACT_APP_API_KEY}`;
-  const fetch_response = await fetch(api_url);
-  const json = await fetch_response.json();
-  res.json(json);
-});
-
-app.get('/logout', function(req, res) {
-  req.session.destroy(function(err){
-    if(err){
-      console.log(err);
-      res.send(err);
-    } else {
-      res.send();
-    }
-  });
-});
-
+routes(app);
 
 //catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -198,12 +61,9 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
 });
-
-/**********/
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
@@ -213,6 +73,5 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
-/**********/
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
